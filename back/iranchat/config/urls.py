cb8c44dd0f9ae51e51205ec50900514e01da651d
sasanzare <sasanzare75@gmail.com>
2022-04-09 14:38:02 +0430

    Edit

--- back/iranchat/config/urls.py
+++ back/iranchat/config/urls.py
@@ -1,64 +1,24 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.views.static import serve
-
-  
-
-
-
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.sitemaps.views import sitemap
-from blog.sitemaps import PostSitemap
 from django.conf import settings
 from django.conf.urls.static import static
 
-# text editor
-# from django.urls import reverse
-# text editor
 
 admin.site.site_header = "پنل ایران چارت"
 admin.site.site_title = "ایران چارت"
 admin.site.index_title = "به پورتال ایران چارت خوش آمدید!"
 
-sitemaps = {
-    'posts': PostSitemap,
-}
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls', namespace='api')),
     path('ckeditor', include('ckeditor_uploader.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
-   
 ]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
-#     urlpatterns += [
-#         re_path(r'^media/(?P<path>.*)$', serve, {
-#             'document_root': settings.MEDIA_ROOT,
-#         }),
-#     ]
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
